50512125c8372f52b08fb7f7483fefa78304ede4
Shyam Khadka <letuscode47@gmail.com>
2025-08-16 18:50:46 +0545

    testing

--- frontend/components/Hero.jsx
+++ frontend/components/Hero.jsx
@@ -76,7 +76,7 @@ const Hero = () => {
               src={slide.image}
               alt={`Slide ${index + 1}`}
               fill
-              className="object-cover object-center"
+              className="object-cover "
               priority={index === 0} // preload the first image for performance
               sizes="(max-width:768px) 22vw , 100vw"
             />
